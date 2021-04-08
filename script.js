63b5f4dd78b31f740b0f88402c9845e42618225d
samComeIt <sanhae1996@gmail.com>
2021-04-08 09:31:00 +0900

    working with classes - classList, multiple functions

--- script.js
+++ script.js
@@ -7,5 +7,30 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 console.log(btnShowModal);
 
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden'); // no dot, dot is only for selector
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < btnShowModal.length; i++)
-  console.log(btnShowModal[i], btnShowModal[i].textContent);
+  btnShowModal[i].addEventListener('click', openModal);
+
+// for (let i = 0; i < btnShowModal.length; i++)
+//   btnShowModal[i].addEventListener('click', function () {
+//     console.log('Button clicked');
+//     modal.classList.remove('hidden'); // no dot, dot is only for selector
+//     overlay.classList.remove('hidden');
+//   });
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+/*btnCloseModal.addEventLister('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});*/
